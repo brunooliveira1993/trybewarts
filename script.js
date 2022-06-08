ff29b7c55e45c0cc0c28be1d8d15da04e9778447
Bruno <brunocfaraco@hotmail.com.br>
2022-06-07 21:38:55 -0300

    modificações no arquivo script.js feitas para arrumar problemas de Lint

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 const loginBtn = document.getElementById('login-button');
-loginBtn.addEventListener('click', function () {
+
+loginBtn.addEventListener('click', () => {
   const email = document.getElementById('email-input').value;
   const password = document.getElementById('password-input').value;
   if (email === 'tryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
-  };
+  }
 });
 
-let digito = document.getElementById('text-area')
-digito.addEventListener('keypress', function (event) {
-  let input = digito.value.length;
-  let maxChars = 500;
-  let contador = document.getElementById('counter')
+const digito = document.getElementById('text-area');
+
+digito.addEventListener('keypress', (event) => {
+  const input = digito.value.length;
+  const maxChars = 500;
+  const contador = document.getElementById('counter');
 
   contador.innerHTML = maxChars - digito.textLength;
 
   if (input >= maxChars) {
     event.preventDefault();
   }
-})
+});
 
-let check = document.getElementById('agreement')
-check.addEventListener('click', function(){
-  let buttonOn = document.getElementById('submit-btn')
-  if(check.checked === true){
-    buttonOn.disabled = false
+const check = document.getElementById('agreement');
 
+check.addEventListener('click', () => {
+  const buttonOn = document.getElementById('submit-btn');
+  if (check.checked === true) {
+    buttonOn.disabled = false;
   }
-})
+});
 
 // contaCaracteres(campo){
 //   var maxChar = 500;
@@ -40,6 +42,3 @@ check.addEventListener('click', function(){
 //   contador.innerHTML = charNumber
 
 // }
-
-
-
